19955c2bbeba33c24236ba93c1c1ac48efab6aef
AayushBhandariii <axyushhh@gmail.com>
2026-09-25 11:50:27 +0545

    Implemented Basic Pong Mechanics with collison and movement

--- pong.c
+++ pong.c
@@ -1,7 +1,8 @@
 #include "raylib.h"
+#include <math.h>
 
-#define PONGRADIUS 10.0f
-#define PLAYERSPEED 100.0f
+#define PONGRADIUS 20.0f
+#define PLAYERSPEED 200.0f
 #define PLAYERECWIDTH 50
 #define PLAYERECHEIGHT 200
 
@@ -10,10 +11,15 @@ const int screenHeight = 800;
 
 int main(){
 
-  Rectangle PlayerOneRec = { (float)screenWidth/2 - 250.0f, (float)screenHeight/2 , PLAYERECWIDTH , PLAYERECHEIGHT};
-  Rectangle PlayerTwoRec = { (float)screenWidth/2 + 200.0f , (float)screenHeight/2 , PLAYERECWIDTH , PLAYERECHEIGHT};
+  Rectangle PlayerOneRec = { (float)screenWidth/2 - 250.0f, (float)screenHeight/2 - (float)PLAYERECHEIGHT/2 , PLAYERECWIDTH , PLAYERECHEIGHT};
+  Rectangle PlayerTwoRec = { (float)screenWidth/2 + 200.0f , (float)screenHeight/2 - (float)PLAYERECHEIGHT/2 , PLAYERECWIDTH , PLAYERECHEIGHT};
 
-    
+  float throwAngle = 30 * (PI/180);
+  float pongInitialVelocity = 400.0f;
+
+  Vector2 PongPosition = { (float)screenWidth/2 , (float)screenHeight/2 };
+  Vector2 PongVelocity = { pongInitialVelocity * cosf(throwAngle) , -pongInitialVelocity*sinf(throwAngle) };
+
   InitWindow(screenWidth, screenHeight, "Pong Game");
   SetTargetFPS(60);
   while(!WindowShouldClose()){
@@ -21,20 +27,13 @@ int main(){
     BeginDrawing();
       ClearBackground(RAYWHITE);
       // For Player 1
-      if(IsKeyDown(KEY_W)){
-        PlayerOneRec.y -= PLAYERSPEED * dt;
-      }
-      if(IsKeyDown(KEY_S)){
-        PlayerOneRec.y += PLAYERSPEED * dt;
-      }
-
+      if(IsKeyDown(KEY_W)) PlayerOneRec.y -= PLAYERSPEED * dt;
+      if(IsKeyDown(KEY_S)) PlayerOneRec.y += PLAYERSPEED * dt;
+      
       // For Player 2 
-      if(IsKeyDown(KEY_UP)){
-        PlayerTwoRec.y -= PLAYERSPEED *  dt;
-      }
-      if(IsKeyDown(KEY_DOWN)){
-        PlayerTwoRec.y += PLAYERSPEED * dt;
-      }
+      if(IsKeyDown(KEY_UP)) PlayerTwoRec.y -= PLAYERSPEED *  dt;
+      if(IsKeyDown(KEY_DOWN)) PlayerTwoRec.y += PLAYERSPEED * dt;
+      
 
       // Collison Detection
       if(PlayerOneRec.y + PLAYERECHEIGHT >= screenHeight) PlayerOneRec.y = screenHeight - PLAYERECHEIGHT ;
@@ -43,6 +42,27 @@ int main(){
 
       if(PlayerTwoRec.y + PLAYERECHEIGHT >= screenHeight) PlayerTwoRec.y = screenHeight - PLAYERECHEIGHT ;
       if(PlayerTwoRec.y <= 0) PlayerTwoRec.y = 0;
+
+
+      // Pong ball movement
+
+      PongPosition.x += PongVelocity.x * dt;
+      PongPosition.y += PongVelocity.y * dt;
+      
+      // Wall Bouncing
+      
+      if((PongPosition.y - PONGRADIUS <= 0) || (PongPosition.x + PONGRADIUS >= screenHeight)){
+        PongVelocity.y = -PongVelocity.y;
+      }
+
+      // Pong Collison handling
+    
+      if(CheckCollisionCircleRec(PongPosition, PONGRADIUS,  PlayerOneRec)) PongVelocity.x = fabsf(PongVelocity.x); 
+      if(CheckCollisionCircleRec(PongPosition, PONGRADIUS,  PlayerTwoRec)) PongVelocity.x = -fabsf(PongVelocity.x); 
+        
+      // Drawing
+
+      DrawCircleV(PongPosition, PONGRADIUS , DARKGREEN);
       DrawRectangle(PlayerOneRec.x, PlayerOneRec.y,PlayerOneRec.width, PlayerOneRec.height, RED);
       DrawRectangle(PlayerTwoRec.x, PlayerTwoRec.y,PlayerTwoRec.width, PlayerTwoRec.height, RED);
     EndDrawing();
